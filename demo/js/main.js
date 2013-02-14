3e313f03b9e97551f85b5a5c94f4642bcbd469bc
James Brady <james.colin.brady@gmail.com>
2013-02-14 08:53:35 +0100

    update demo to check isFile after capture

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,12 +1,35 @@
-$(document).bind('touchend', function () {
-    forge.internal.call('multi_image_select.getImages', {}, function(results) {
-        for (var file in results) {
-            forge.file.URL(results[file], function (url) {
-                var img = $("<img />").attr({ "src": url }).css({ "max-width": "80%" });
-                img.appendTo("#selection");
-            });
-        }
-    }, function(e) { 
-        alert("failed: " + e.message);
+$(function () {
+    function showImage (file, extraCss) {
+        extraCss = extraCss === undefined? {}: extraCss;
+        forge.file.URL(file, function (url) {
+            var img = $("<img />").attr({ "src": url }).css({ "max-width": "30%" }).css(extraCss);
+            $('#images').prepend(img);
+        });
+    }
+    function dumpImageData (file, logTag) {
+        forge.file.isFile(file, function (isFile) {
+            forge.logging.info(logTag + " " + JSON.stringify(file)+" is file: "+isFile);
+        });
+    }
+    $('a.multi').on('click', function () {
+        forge.internal.call('multi_image_select.getImages', {}, function(results) {
+            for (var idx=0; idx<results.length; idx++) {
+                var file = results[idx];
+                showImage(file, {'border': '2px dotted red'});
+                dumpImageData(file, "multi");
+            }
+        }, function (e) {
+            alert("plugin failed: " + e.message);
+        });
+
+    });
+    $('a.getImage').on('click', function () {
+        forge.file.getImage(function (file) {
+            showImage(file, {'border': '2px dotted green'});
+            dumpImageData(file, "getImage");
+
+        }, function (e) {
+            alert("file.getImage failed: "+e.message);
+        });
     });
 });
